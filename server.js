7f6728769071a6b1a839601865bd15680ecc4585
MiloAillo <panuwucake@gmail.com>
2025-03-26 20:05:26 +0700

    Cleaning the clutter

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
 import express from 'express';
-const app = express()
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
-const port = process.env.PORT || '3000'
-app.listen(port, () => console.log(`listening to port ${port}`));
-
+import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+
+const app = express()
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-import path from 'path';
-app.use(express.static(path.join(__dirname, 'public')));
 
+const port = process.env.PORT || '3000'
+app.listen(port, () => console.log(`listening to port ${port}`));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-
-const data = []
 
 app.get('/request', (req, res) => {
     console.log('client-side request arrived!')
     const city = req.query.city;
     console.log(city);
-    data.push(city)
     const value = encodeURIComponent(city)
     const url = `http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=1&appid=${process.env.API_KEY}`
     getWeather(url)
-    console.log("current database: " + data);
 
     async function getWeather(url) {
         try {
